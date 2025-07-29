d8e5c05d2db93f7be28b36d2829d845986125976
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-07-29 14:34:15 -0700

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,15 +1,15 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name                 = var.vpc_name
-  cidr                 = var.vpc_cidr
-  azs                  = var.azs
-  public_subnets       = var.public_subnets
-  private_subnets      = var.private_subnets
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_support   = true
+  name = var.name
+  cidr = var.cidr_block
+
+  azs             = var.azs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
   enable_dns_hostnames = true
 
   tags = var.tags
