15056504a8b4f800352b69e9dec8c74ef5c7da49
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-07-29 14:36:47 -0700

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,19 +1,26 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+module "vpc" {
+  source = "../../modules/vpc"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  vpc_id          = var.vpc_id
-  subnet_ids      = var.subnet_ids
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-      instance_types = ["t3.medium"]
-    }
+  name            = "dev-vpc"
+  cidr_block      = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
   }
+}
 
-  tags = var.tags
+module "eks" {
+  source  = "../../modules/eks"
+
+  cluster_name    = "dev-eks-cluster"
+  cluster_version = "1.28"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
