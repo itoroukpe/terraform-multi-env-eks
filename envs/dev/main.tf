dc37bba3eacc9b0c793fbf8c854e097fc54c54e3
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-07-28 22:28:12 -0700

    Create main.tf

---
+++ envs/dev/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_name        = "dev-vpc"
+  vpc_cidr        = "10.10.0.0/16"
+  azs             = ["us-west-2a", "us-west-2b"]
+  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
+  tags            = { Environment = "dev" }
+}
+
+module "eks" {
+  source       = "../../modules/eks"
+  cluster_name = "dev-eks-cluster"
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+  tags         = { Environment = "dev" }
+}
