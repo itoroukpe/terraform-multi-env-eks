619bb7af69b3fe4ff6e3c8636f78a41a728a38e9
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-08-02 06:58:29 -0700

    Update main.tf

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,26 +1,20 @@
-module "vpc" {
-  source = "../../modules/vpc"
-
-  name            = "dev-vpc"
-  cidr_block      = "10.0.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
-
 module "eks" {
-  source  = "../../modules/eks"
-
-  cluster_name    = "dev-eks-cluster"
-  cluster_version = "1.28"
-  vpc_id          = module.vpc.vpc_id
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 20.0" # use latest compatible version
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
   subnet_ids      = module.vpc.private_subnets
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
+  vpc_id          = module.vpc.vpc_id
+
+  enable_irsa = true
+
+  # Worker group configuration
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
   }
 }
