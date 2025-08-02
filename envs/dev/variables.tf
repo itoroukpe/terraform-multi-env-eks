2354472e815f617b6b7b74fef76194c82e70aadc
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2025-08-02 06:58:57 -0700

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,4 +1,9 @@
-variable "region" {
-  description = "AWS region to deploy resources into"
+variable "cluster_name" {
   type        = string
+  default     = "dev-eks-cluster"
+}
+
+variable "cluster_version" {
+  type        = string
+  default     = "1.29"
 }
